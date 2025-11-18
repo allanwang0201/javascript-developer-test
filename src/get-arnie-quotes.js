00f2ab5d894817e855a19e8d95310cd5c4fcb9a7
“Allan” <allanwang0201@gmail.com>
2025-11-18 19:31:52 +1100

    first commit

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,11 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const responses = await Promise.all(urls.map(httpGet));
+  return responses.map(({ status, body }) => {
+    const { message } = JSON.parse(body);
+    return status === 200 ? { 'Arnie Quote': message } : { FAILURE: message };
+  });
 };
 
 module.exports = {
